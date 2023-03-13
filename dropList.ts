2a83056df90019b3dfa45ede8b655b504554e357
Alexander Penado <59466352+Jose-source@users.noreply.github.com>
2023-03-12 17:11:14 -0700

    Drop List template & Script

---
+++ dropList.ts
@@ -0,0 +1,7 @@
+export interface Drop {
+    walletAddress: string,
+    numLamports: number
+}
+export const dropList:Drop[] = [
+    {walletAddress:"addPublicReceipientWalletAddress",numLamports:987654321}
+];
